e4339cb4e7f7193e247a14575c1f2c84824911c7
Gui <eu@guidf.com>
2026-03-14 23:17:24 -0300

    User Flags # NO_TRANSFER

--- website/src/lib/data/flags.ts
+++ website/src/lib/data/flags.ts
@@ -3,7 +3,8 @@ export const UserFlags = {
 	IS_HEAD_ADMIN: 1n << 1n,
 	FOUNDER_BADGE: 1n << 2n,
 	NO_ARCADE: 1n << 3n,
-	NO_TRADING: 1n << 4n
+	NO_TRADING: 1n << 4n,
+    NO_TRANSFER: 1n << 5n
 } as const;
 export function hasFlag(
 	_flags: bigint | number | string | undefined,
